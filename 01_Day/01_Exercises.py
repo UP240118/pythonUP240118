c8f0fc83b9c879f7ec7f90c3c89c1f32556c1076
UP240118 <up240118@alumnos.upa.edu.mx>
2025-02-27 13:27:31 -0600

    Dia1

---
+++ 01_Day/01_Exercises.py
@@ -0,0 +1,45 @@
+# Exercices 1
+a = 3
+b = 4
+#Suma
+c=a+b
+print("La suma de A y B es = ", c)
+#Resta
+c=a-b
+print("La resta de A y B es = ", c)
+#Multiplicacion
+c=a*b
+print("La multiplicacion de A y B es = ", c)
+#Porcentaje
+c=a%b
+print("El procentaje de A y B es = ", c)
+#Division
+c=a/b
+print("La division de A y B es = ", c)
+#Exponencial
+c=a**b
+print("El exponencial de A y B es = ", c)
+
+print("//////////////////////////////////////////////////////////////")
+n = input(" Ingrsa tu Nombre = ")
+m = input(" Ingrsa el Nombre de tu Famlia separado por comas = ")
+o = input(" Ingrsa tu Lugar de Residencia = ")
+p = input(" Dime si te gusta Python = ")
+
+print(" Tu nombre es = ",n)
+print(" El Nombre de tu Familia es = ",m)
+print(" Vives en = ",o)
+print(" Al parecer Pyton = ",p)
+#//////////////////////////////////Ejercicio 2/////////////////////////////////////
+print("//////////////////////////////////////////////////////////////")
+
+print(type(n))
+print(type(m))
+print(type(o))
+print(type(9.18))
+print(type(3.14))
+print(type(4 - 4j))
+print(type("Python"))
+#//////////////////////////////////Ejercicio 3/////////////////////////////////////
+print("//////////////////////////////////////////////////////////////")
+
