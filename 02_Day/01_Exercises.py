c83903e80e03d79e52470abff2b28a2438d79e70
UP240118 <up240118@alumnos.upa.edu.mx>
2025-02-27 19:02:37 -0600

    #Exercice_2

---
+++ 02_Day/01_Exercises.py
@@ -0,0 +1,33 @@
+#Exerccices 02__Variables
+Primer_Nombre = "Axel"
+Segundo_Nombre = "Yael"
+Apellidos = "Martínez Martínez"
+Nombre_Completo = "Axel Yael Martínez Martínez"
+
+Pais_de_Origen = "Estados Unidos Mexicanos"
+Ciudad_de_Recidencia = "Aguascalientes, Ags."
+
+Edad = "18 Años"
+Año_en_Curso = "2025" 
+
+is_married = "No esta Casado"
+is_true = True
+
+Gustos_Personales = {
+    "Armar Model kits", 
+    "Escuchar a Ado",
+    "Cocinar", 
+    "Dibujar",
+    "Leer Manga",
+    "Jugar Video Juegos"
+}
+#Imprimir las Variables
+print("Nombres =" , Primer_Nombre, Segundo_Nombre)
+print("Apellidos =" , Apellidos)
+print("Nombre Completos =" , Nombre_Completo)
+print("Pais de Origen =" , Pais_de_Origen)
+print("Ciudad de Recidencia =" , Ciudad_de_Recidencia)
+print("Edad =" , Edad)
+print("Año =" , Año_en_Curso)
+print("Esta Casado =" , is_married)
+print("Gustos Personales =" , Gustos_Personales)
