fed8d8d70ac910a28c726ad66b75abff5c9ee223
UP240118 <up240118@alumnos.upa.edu.mx>
2025-03-06 12:53:35 -0600

    #Day_04

---
+++ 04_Day/01_Exercises.py
@@ -0,0 +1,55 @@
+# 1
+a = "Thrity" 
+b = "Days"
+c = "Of"
+d = "Python"
+print (a, b, c, d)
+
+# 2
+e = "Coding"
+f = "For"
+g = "All"
+print(e, f, g)
+
+# 3, 4, 5
+Company = "Coding For All"
+print (Company, len(Company))
+
+# 6, 7, 8
+h = Company.upper()
+i = Company.lower()
+print (h,"-",i)
+print (Company.capitalize(),"-",Company.title(),"-",Company.swapcase())
+
+# 9
+Company1 = "Coding For All"
+Company1 = Company1.strip("Coding")
+print(Company1)
+
+# 10
+print("Coding" in Company)
+
+# 11, 12, 13, 14
+Company = Company.replace("Coding", "Python")
+print(Company)
+
+Company = Company.split()
+print(Company)
+
+C1 = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+C1 = C1.split(",")
+print(C1)
+
+# 15, 16, 17
+C2 = "Coding For All"
+C2 = C2[0]
+print(C2)
+
+C3 = "Coding For All"
+C3 = C3[-1]
+print(C3)
+
+C4 = "Coding For All"
+C4 = C4[10]
+print(C4)
+
