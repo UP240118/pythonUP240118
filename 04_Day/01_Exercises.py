78282d7bb474bdc34810eb8766259d662be98022
UP240118 <up240118@alumnos.upa.edu.mx>
2025-03-09 14:19:25 -0600

    #u

--- 04_Day/01_Exercises.py
+++ 04_Day/01_Exercises.py
@@ -53,3 +53,54 @@ C4 = "Coding For All"
 C4 = C4[10]
 print(C4)
 
+#18, 19
+C5 = "Python For Everyone"
+C5 = C5.split()
+Ac1 = ""
+for C5 in C5:
+  Ac1 += C5[0].upper()
+print(Ac1)
+
+C6 = "Coding For All"
+C6 = C6.split()
+Ac2 = ""
+for C6 in C6:
+    Ac2 += C6[0].upper()
+print(Ac2)
+
+#20, 21, 22
+C = "Coding For All"
+print(C.index("C"))
+print(C.index("F"))
+print(C.rfind("l"))
+
+#23, 24, 25, 26
+B = "You cannot end a sentence with because because because is a conjunction"
+print(B.index("because"))
+print(B.rindex("because"))
+print(B.split("because"))
+print(B.find("because"))
+
+#28, 29
+text = "Coding For All"
+result = text.startswith("Coding")
+print(result)
+
+text = "Coding For All"
+result = text.endswith("Coding")
+print(result)
+
+#30
+text = " Coding For All "
+result = text.strip(" ")
+print(result)
+
+#31
+D = "30DaysOfPython"
+t = "tirthy_day_of_python"
+print(D.isidentifier())
+print(t.isidentifier())
+
+#32
+
+#33
