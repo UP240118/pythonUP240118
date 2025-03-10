25e3705afc93e7d1d6f7fa34079508648572dfc5
UP240118 <up240118@alumnos.upa.edu.mx>
2025-03-10 12:38:47 -0600

    #Day_06

---
+++ 06_Day/01_Exercises.py
@@ -0,0 +1,17 @@
+#1, 2, 3
+empty_tuple = ()
+Primos = "Corazon", "Aranza", "Denise", "Estrella"
+Hermana = "Laila"
+Hermano = "Pablo"
+show_family = Hermano + Hermana
+print (show_family)
+
+#4
+Pr = len(Primos)
+print (f"Yo Tengo {Pr} Primos")
+
+#5
+M = "Mayra"
+P = "Juan Carlos"
+F = (M, P) + show_family
+print (f"Mi Mama se llama {mom}, Mi Papa se llama {dad}, Mis primos son {show_family}")
